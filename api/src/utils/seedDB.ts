030371c53193aa516d72a09f5db3afd38e783e7d
mohamed nagah <mohamed.nagah95@gmail.com>
2022-07-17 15:22:09 +0200

    add database seeding

---
+++ api/src/utils/seedDB.ts
@@ -0,0 +1,50 @@
+import dotenv from 'dotenv'
+import { PrismaClient, TicketStatus, User } from '@prisma/client'
+import { faker } from '@faker-js/faker'
+import { join } from 'path'
+dotenv.config({ path: join(__dirname, '..', '..', '.env') })
+
+const ticketStatusArray = Object.keys(TicketStatus)
+
+const seedDB = async () => {
+  const prisma = new PrismaClient()
+
+  const users: User[] = []
+
+  await prisma.ticketHistory.deleteMany()
+  await prisma.ticket.deleteMany()
+  await prisma.user.deleteMany()
+
+  // create 10 fake users
+  for (let i = 0; i < 10; i++) {
+    const user = await prisma.user.create({
+      data: {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        username: faker.internet.userName(),
+        password: faker.internet.password(),
+      },
+    })
+    users.push(user)
+  }
+
+  for (let i = 0; i < 60; i++) {
+    await prisma.ticket.create({
+      data: {
+        title: faker.random.words(4),
+        description: faker.random.words(20),
+        status: (ticketStatusArray[
+          Math.floor(Math.random() * ticketStatusArray.length)
+        ] || TicketStatus.TODO) as TicketStatus | undefined,
+        assignedUserId: users[Math.floor(Math.random() * users.length)].id,
+        createdAt: faker.date.past(),
+        updatedAt: faker.date.past(),
+        authorId: users[Math.floor(Math.random() * users.length)].id,
+        lastUpdatedByUserId: users[Math.floor(Math.random() * users.length)].id,
+      },
+    })
+  }
+}
+
+seedDB()
